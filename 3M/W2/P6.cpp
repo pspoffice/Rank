c7511b9943a0797115e984262d65a67f44ff0eff
Priyank S Patel <pspoffice365@gmail.com>
2022-04-08 11:11:58 +0530

    Week 2

--- 3M/W2/P6.cpp
+++ 3M/W2/P6.cpp
@@ -1,4 +1,4 @@
-// https://www.hackerrank.com/challenges/three-month-preparation-kit-countingsort1/problem?isFullScreen=true&h_l=interview&playlist_slugs%5B%5D=preparation-kits&playlist_slugs%5B%5D=three-month-preparation-kit&playlist_slugs%5B%5D=three-month-week-two
+// https://www.hackerrank.com/challenges/three-month-preparation-kit-counting-valleys/problem?isFullScreen=true&h_l=interview&playlist_slugs%5B%5D=preparation-kits&playlist_slugs%5B%5D=three-month-preparation-kit&playlist_slugs%5B%5D=three-month-week-two
 
 #include <bits/stdc++.h>
 using namespace std;
